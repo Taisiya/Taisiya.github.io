bf51dfb30558e3e060d5fb76cac831464a58c953
Taisiya <kurmaevataisija@mail.ru>
2014-03-09 16:13:44 +0200

    commit 5

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+// Обязательная обёртка
+module.exports = function(grunt) {
+
+    // Задачи
+    grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+        // Склеиваем
+        concat: {
+            main: {
+                src: [
+					'js/libs/*.js',
+                    'js/**/*.js'  // Все JS-файлы в папке
+                ],
+                dest: 'build/scripts.js'
+            }
+        },
+        // Сжимаем
+        uglify: {
+            main: {
+                files: {
+                    // Результат задачи concat
+                    'build/scripts.min.js': '<%= concat.main.dest %>'
+                }
+            }
+        }
+    });
+
+    // Загрузка плагинов, установленных с помощью npm install
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Задача по умолчанию
+    grunt.registerTask('default', ['concat', 'uglify']);
+};
